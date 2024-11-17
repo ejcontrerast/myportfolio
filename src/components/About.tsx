2997b13eaa9df9f7cd1b6e488fff50205ae576ea
Ernesto <ejcontrerast@outlook.com>
2024-11-16 19:03:52 -0800

    Feat: I deleted the photo and added to the about component, refined buttons and adjusted the responsivness of the component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,10 +3,19 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <section id="about" className="p-8 text-center">
-      <h2 className="text-3xl font-bold">About Me</h2>
-      <p className="mt-4 max-w-2xl mx-auto">
-        I’m a developer dedicated to creating modern, user-friendly web applications...
-      </p>
+      <div>
+        {/* Photo */}
+        <div className="w-40 h-40 mb-6 rounded-full overflow-hidden shadow-lg hidden sm:block">
+          <img src="src\assets\Photo.png" className='w-full h-full object-cover'></img>
+        </div>
+        
+        <div>
+          <h2 className="text-3xl font-bold">About Me</h2>
+          <p className="mt-4 max-w-2xl mx-auto">
+            I’m a developer dedicated to creating modern, user-friendly web applications...
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,39 +3,39 @@ import React from 'react';
 
 const Hello: React.FC = () => {
   return (
-    <section id="hello" className="flex flex-grow-0 items-center justify-center h-3/4 text-center p-8 gap-10 dark:bg-dark-background bg-custompattern bg-light-background">
-      {/* Photo */}
-      <div className="w-40 h-40 mb-6 rounded-full overflow-hidden shadow-lg hidden sm:block">
-        <img src="src\assets\Photo.png" className='w-full h-full object-cover'></img>
-      </div>
-
+    <section id="hello" className="flex items-center justify-center text-center sm:h-3/4 max-h-screen px-5 gap-10
+                                  dark:bg-dark-background bg-light-background bg-custompattern">
       {/* Text + buttons */}
       <div className='flex flex-col gap-6 py-6 mt-20'>
 
         {/* Text */}
         <div className='dark:text-dark-primary text-light-primary'>
-          <h1 className="text-4xl font-bold dark:text-dark-secondary text-light-secondary">
-            <span className="">Hello!</span> Welcome to my portfolio!</h1>
-          <p className="text-lg mt-4 dark:text-dark-primary text-light-primary"><p>I’m excited to share my journey in IT, web development, and data analytics with you. Take a look around to discover my projects, skills, and vision for creating impactful technology solutions.</p>
-          Thanks for stopping by!</p>
+          <h1 className="text-4xl font-bold dark:text-dark-secondary text-light-secondary pb-3">
+            Hello! Welcome to my portfolio!
+          </h1>
+          <p className="text-lg dark:text-dark-primary text-light-primary px-2 sm:px-6 md:px-20 lg:px-52 2xl:px-72"><p>I’m Ernesto, and am excited to share my journey in IT, web development, and data analytics with you. Take a look around to discover my projects, skills, and vision for creating impactful technology solutions.</p>
+            Thanks for stopping by!
+          </p>
         </div>
 
         {/* Buttons Resume and Projects*/}
         <div className="flex items-center justify-center space-x-4 ">
           <a 
-            href="/path-to-your-resume.pdf" 
+            href="/path-to-your-resume.pdf"
             download 
-            className="px-6 py-3 border flex
+            className="px-3 py-2 font-medium border focus:ring-2
                        dark:focus:ring-dark-accent dark:bg-transparent dark:border-dark-accent/70 dark:text-dark-accent/70 dark:hover:bg-dark-accent/60 dark:hover:text-dark-primary 
-                       bg-light-accent text-light-secondary font-medium rounded-md shadow-md hover:bg-accent/80 focus:outline-none focus:ring-2 focus:ring-accent"
+                       focus:ring-light-accent bg-transparent border-light-secondary text-light-secondary hover:border-light-primary hover:bg-light-accent/80 hover:text-light-background
+                       rounded-sm shadow-md"
           >
             Resume   
           </a>
           <a 
             href="#projects" 
-            className="px-6 py-3 border 
-                    dark:focus:ring-dark-accent dark:bg-dark-accent dark:border-dark-secondary bg-light-accent dark:text-dark-background dark:hover:bg-dark-secondary/60 dark:hover:text-dark-primary 
-                    text-light-secondary bg-secondary text-background font-medium rounded-md shadow-md hover:bg-secondary/80 focus:outline-none focus:ring-2 focus:ring-secondary"
+            className="px-3 py-2 font-medium border focus:ring-2
+                      dark:focus:ring-dark-accent dark:bg-dark-accent dark:border-dark-secondary dark:text-dark-background dark:hover:bg-dark-secondary/60 dark:hover:text-dark-primary 
+                      focus:ring-light-accent bg-light-accent border-light-accent text-light-secondary hover:border-light-accent hover:bg-light-primary hover:text-light-additional
+                      rounded-sm shadow-md"
           >
             Projects
           </a>
